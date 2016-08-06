0345d6b560d4d2582af20a779174b8ba8b73b7d0
Tony McCormick <tony@mi-squared.com>
2016-08-05 19:21:57 -0700

    Remove obsolete 3rd party code

--- interface/main/left_nav.php
+++ interface/main/left_nav.php
@@ -272,11 +272,8 @@ function genPopupsList($style='') {
 <?php } ;
  if (!$GLOBALS['disable_calendar']) { ?>
  <option value='../reports/appointments_report.php?patient=<?php if(isset($pid)) {echo $pid;} ?>'><?php xl('Appts','e'); ?></option>
-<?php } ;
- if (file_exists("$webserver_root/custom/refer.php")) { ?>
- <option value='../../custom/refer.php'><?php xl('Refer','e'); ?></option>
 <?php } ?>
- <option value='../patient_file/printed_fee_sheet.php?fill=1'><?php xl('Superbill','e'); ?></option>
+<option value='../patient_file/printed_fee_sheet.php?fill=1'><?php xl('Superbill','e'); ?></option>
  <option value='../patient_file/front_payment.php'><?php xl('Payment','e'); ?></option>
 <?php if ($GLOBALS['inhouse_pharmacy']) { ?>
  <option value='../patient_file/pos_checkout.php'><?php xl('Checkout','e'); ?></option>
@@ -983,10 +980,6 @@ function removeOptionSelected(EncounterId)
    if (opt.text == 'Export' || opt.text == 'Import') {
     width  = 500;
     height = 400;
-   }
-   else if (opt.text == 'Refer') {
-    width  = 700;
-    height = 500;
    }
    dlgopen(opt.value, '_blank', width, height);
   }
@@ -1696,14 +1689,6 @@ if (!empty($reg)) {
 <?php } ?>
 
 <br /><hr />
-
-<?php
- // To use RelayHealth, see comments and parameters in includes/config.php.
- if (!empty($GLOBALS['ssi']['rh'])) {
-  include_once("../../library/ssi.inc");
-  echo getRelayHealthLink() ."<br /><hr />\n";
- }
-?>
 
 <div id='current_patient' style = 'display:none'>
 <b><?php xl('None','e'); ?></b>
