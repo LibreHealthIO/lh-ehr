394e66d3642ef714a061d6bf9eac0a9187e133e5
Tony McCormick <tony@mi-squared.com>
2016-08-16 11:26:36 -0700

    Add Last,First and PID to Patient Dynamic Search - issue 97

--- sql/libreehr_upgrade.sql
+++ sql/libreehr_upgrade.sql
@@ -1,8 +1,9 @@
-RENAME `openemr_module_vars` TO `libreehr_module_vars`;
-RENAME `openemr_modules` TO `libreehr_modules`;
-RENAME `openemr_postcalendar_categories` TO `libreehr_postcalendar_categories`;
-RENAME `openemr_postcalendar_events` TO `libreehr_postcalendar_events`;
-RENAME `openemr_postcalendar_limits` TO `libreehr_postcalendar_limits`;
-RENAME `openemr_postcalendar_topics` TO `libreehr_postcalendar_topics`;
-RENAME `openemr_session_info` TO `libreehr_session_info`;
-RENAME `openemr_module_vars` TO `libreehr_module_vars`;
+# This is only need on existing OpenEMR sites
+RENAME TABLE `openemr_module_vars` TO `libreehr_module_vars`;
+RENAME TABLE `openemr_modules` TO `libreehr_modules`;
+RENAME TABLE `openemr_postcalendar_categories` TO `libreehr_postcalendar_categories`;
+RENAME TABLE `openemr_postcalendar_events` TO `libreehr_postcalendar_events`;
+RENAME TABLE `openemr_postcalendar_limits` TO `libreehr_postcalendar_limits`;
+RENAME TABLE `openemr_postcalendar_topics` TO `libreehr_postcalendar_topics`;
+RENAME TABLE `openemr_session_info` TO `libreehr_session_info`;
+RENAME TABLE `openemr_module_vars` TO `libreehr_module_vars`;
