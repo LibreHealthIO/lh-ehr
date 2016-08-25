56309626eddd17559829543608e9f5d295ee23d3
Tony McCormick <tony@mi-squared.com>
2016-08-25 15:51:07 -0700

    Cleanup contribs, remove forms and other deprecated stuff - issue #110 (#126)

--- contrib/forms/evaluation/new.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("../../globals.php");
-include_once("$srcdir/api.inc");
-
-require ("C_FormEvaluation.class.php");
-
-$c = new C_FormEvaluation();
-echo $c->default_action();
-?>
