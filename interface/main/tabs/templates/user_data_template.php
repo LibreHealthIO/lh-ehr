19c7bd91a0dcf6725fef893e2ac38735eb651023
teryhill <teryhill@yahoo.com>
2016-08-31 17:52:58 -0400

    # 142 Added a development Flag
    Used to turn options on and off for development Used to turn off left
    nav untill we decide to eliminate it.

--- interface/main/tabs/templates/user_data_template.php
+++ interface/main/tabs/templates/user_data_template.php
@@ -5,7 +5,9 @@
             <span data-bind="text:fname"></span>
             <span data-bind="text:lname"></span>
             <div class="userfunctions">
+			    <?php if ($GLOBALS['development_flag'] ) { ?>
                 <div data-bind="click: framesMode"><?php echo xlt("Frames Mode");?></div>
+				<?php } ?>
                 <div data-bind="click: changePassword"><?php echo xlt("Change Password");?></div>
                 <div data-bind="click: logout"><?php echo xlt("Logout");?></div>
                 
