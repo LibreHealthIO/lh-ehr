18c2621262cacbc8745759041022c50093c9a9ab
Terry Hill <teryhill@yahoo.com>
2016-11-21 14:55:33 -0500

    Misc_Billing_Form Changes. (#274)
    * Misc_Billing_Form Changes. These changes allow the EPSDT options, The box 17 options, The
    electronic resubmission of claims.
    * Add provider list

--- interface/forms/misc_billing_options/view.php
+++ interface/forms/misc_billing_options/view.php
@@ -1,3 +1,35 @@
 <?php
+/*
+ *  view.php for the viewing of information from the misc_billing_form
+ *
+ *  This program views the misc_billing_form
+ *  The changes to this file as of November 16 2016 to view misc_billing_form
+ *  are covered under the terms of the Mozilla Public License, v. 2.0
+ *
+ * @copyright Copyright (C) 2016 Terry Hill <terry@lillysystems.com>
+ *
+ *
+ * LICENSE: This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://opensource.org/licenses/gpl-license.php.
+ *
+ * LICENSE: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * See the Mozilla Public License for more details.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * @package LibreEHR
+ * @author Terry Hill <terry@lilysystems.com>
+ * @link http://www.libreehr.org
+ *
+ * Please help the overall project by sending changes you make to the author and to the LibreEHR community.
+ *
+ */
 require("new.php");
 ?>
--- sql/database.sql
+++ sql/database.sql
@@ -1305,6 +1305,10 @@ CREATE TABLE `form_misc_billing_options` (
   `auto_accident` tinyint(1) default NULL,
   `accident_state` varchar(2) default NULL,
   `other_accident` tinyint(1) default NULL,
+  `medicaid_referral_code` varchar(2)   default NULL,
+  `epsdt_flag` tinyint(1) default NULL,
+  `provider_qualifier_code` varchar(2) default NULL,
+  `provider_id` int(11) default NULL,
   `outside_lab` tinyint(1) default NULL,
   `lab_amount` decimal(5,2) default NULL,
   `is_unable_to_work` tinyint(1) default NULL,
@@ -1319,6 +1323,7 @@ CREATE TABLE `form_misc_billing_options` (
   `prior_auth_number` varchar(20) default NULL,
   `comments` varchar(255) default NULL,
   `replacement_claim` tinyint(1) default 0,
+  `icn_resubmission_number` varchar(35) default NULL,
   `box_14_date_qual` char(3) default NULL,
   `box_15_date_qual` char(3) default NULL,
   PRIMARY KEY  (`id`)
@@ -4317,6 +4322,11 @@ INSERT INTO list_options (list_id, option_id, title, notes, seq) VALUES ('Immuni
 INSERT INTO list_options (list_id, option_id, title, notes, seq) VALUES ('Immunization_Completion_Status','Not_Administered','Not Administered','NA', '30');
 INSERT INTO list_options (list_id, option_id, title, notes, seq) VALUES ('Immunization_Completion_Status','Partially_Administered','Partially Administered','PA', '40');
 
+-- provider_qualifier_code
+
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('lists','provider_qualifier_code','Provider Qualifier Code', 1,0);
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('provider_qualifier_code','dk','DK',10,0);
+INSERT INTO list_options ( list_id, option_id, title, seq, is_default ) VALUES ('provider_qualifier_code','dn','DN',20,0);
 
 -- 
 -- Table structure for table `lists`
