43a3462376980ae944ef0291be0b9156f1ce5f83
Ujjwal Arora <arora.ujjwal@gmail.com>
2017-06-22 19:50:00 -0700

    Remove catid from new event creation because it's not used

--- interface/main/calendar_new/index.php
+++ interface/main/calendar_new/index.php
@@ -146,10 +146,10 @@ require('includes/session.php');
               alert('There was an error while fetching appointments.');
           }
         },
-        select: function(start, end, jsEvent, view, resource) { //TODO: FIX
+        select: function(start, end, jsEvent, view, resource) {
           dlgopen('../calendar/add_edit_event.php?' + '&starttimeh=' + start.get('hours') + '&userid=' + resource.id + 
-          '&starttimem=' + start.get('minutes') + '&date=' + start.format('YYYYMMDD')+ '&catid=' + 0
-           <?php
+          '&starttimem=' + start.get('minutes') + '&date=' + start.format('YYYYMMDD') // + '&catid=' + 0
+           <?php // TODO: This part (modified) goes in add edit event
             if(isset($_SESSION[pid]))
                 {
                     if($_SESSION[pid]>0)
