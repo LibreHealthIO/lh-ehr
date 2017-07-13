aaf7977398cc8874fca69711174443c34fcdf20c
Tony McCormick <tony@mi-squared.com>
2017-07-13 14:51:40 -0700

    add check for inactive

--- interface/eRx_xml.php
+++ interface/eRx_xml.php
@@ -229,18 +229,18 @@ function account($doc,$r)
             $doc->createTextNode( $erxSiteID['state'] )
         );
         $AccountAddress->appendChild( $state );
-        $jasonbigzip=$erxSiteID['postal_code'];
-    $jasonbigzip=preg_replace('/[^0-9]/','',$jasonbigzip);
-    if(strlen($jasonbigzip) >=5){
-        $jasonzip=substr($jasonbigzip,0,5);
-        $zip4=substr($jasonbigzip,5,4);
+        $jsonbigzip=$erxSiteID['postal_code'];
+    $jsonbigzip=preg_replace('/[^0-9]/','',$jsonbigzip);
+    if(strlen($jsonbigzip) >=5){
+        $jsonzip=substr($jsonbigzip,0,5);
+        $zip4=substr($jsonbigzip,5,4);
     }
     else{
-        $msg = validation(xl('Facility Zip'),$jasonzip,$msg);
+        $msg = validation(xl('Facility Zip'),$jsonzip,$msg);
     }
         $zip = $doc->createElement( "zip" );
         $zip->appendChild(
-            $doc->createTextNode( $jasonzip )
+            $doc->createTextNode( $jsonzip )
         );
         $AccountAddress->appendChild( $zip );
     if(strlen($zip4)==4){
@@ -278,6 +278,7 @@ function account($doc,$r)
 function location($doc,$r)
 {
     global $msg;
+    $jsonzip="";
     $userRole=sqlQuery("SELECT * FROM users AS u LEFT JOIN facility AS f ON f.id=u.facility_id WHERE u.username=?",array($_SESSION['authUser']));
     $b = $doc->createElement( "Location" );
     $b->setAttribute('ID',$userRole['id']);
@@ -312,18 +313,18 @@ function location($doc,$r)
         );
         $LocationAddress->appendChild($state);
         }
-    $jasonbigzip=$userRole['postal_code'];
-    $jasonbigzip=preg_replace('/[^0-9]/','',$jasonbigzip);
-    if(strlen($jasonbigzip) >=5){
-        $jasonzip=substr($jasonbigzip,0,5);
-        $zip4=substr($jasonbigzip,5,4);
+    $jsonbigzip=$userRole['postal_code'];
+    $jsonbigzip=preg_replace('/[^0-9]/','',$jsonbigzip);
+    if(strlen($jsonbigzip) >=5){
+        $jsonzip=substr($jsonbigzip,0,5);
+        $zip4=substr($jsonbigzip,5,4);
     }
     else{
-        $msg = validation(xl('Facility Zip'),$jasonzip,$msg);
+        $msg = validation(xl('Facility Zip'),$jsonzip,$msg);
     }
         $zip = $doc->createElement( "zip" );
         $zip->appendChild(
-            $doc->createTextNode( $jasonzip )
+            $doc->createTextNode( $jsonzip )
         );
         $LocationAddress->appendChild( $zip );
     if(strlen($zip4)==4){
@@ -784,7 +785,7 @@ function PatientMedication($doc,$r,$pid,$med_limit)
             $b->appendChild( $sig );
             $dispenseNumber = $doc->createElement( "dispenseNumber" );
             $dispenseNumber->appendChild(
-                $doc->createTextNode( $prec['quantity'] )
+                $doc->createTextNode( $prec['quantity'] )  // THIS CAN't BE RIGHT tdm $prec is outside of scope
             );
             $b->appendChild( $dispenseNumber );
             $sig = $doc->createElement( "sig" );
@@ -863,6 +864,7 @@ function PatientFreeformHealthplans($doc, $r, $pid) {
             WHERE `id`.`pid` = ?
                 AND `id`.`subscriber_relationship` = \'self\'
                 AND `id`.`provider` > 0
+                AND `id`.`inactive` = 0
             ORDER BY `id`.`date` DESC
             LIMIT 3
         ) AS `ins`
