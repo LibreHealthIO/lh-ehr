f3c604c51d5ae6c0d998c3ea3d1524b308e0dd4f
Art Eaton <Art@starfrontiers.org>
2017-07-18 23:46:46 -0400

    Clinical Rules Class Attribution cleanup
    Added appropriate attribution to files

--- library/classes/rulesets/PQRS/PQRSReportFactory.php
+++ library/classes/rulesets/PQRS/PQRSReportFactory.php
@@ -2,24 +2,19 @@
 /**
  * Report Factory for PQRS Measures
  *
- * Copyright (C) 2016      Suncoast Connection
+ * Copyright (C) 2015 - 2017      Suncoast Connection
+  * 
+ * LICENSE: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0
+ * See the Mozilla Public License for more details. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * 
+ * @author  Art Eaton <art@suncoastconnection.com>
+ * @author  Bryan lee <bryan@suncoastconnection.com>
+ * @package LibreHealthEHR 
+ * @link    http://suncoastconnection.com
+ * @link    http://librehealth.io
  *
- * LICENSE: This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://opensource.org/licenses/gpl-license.php>.
- *
- * @package OpenEMR
- * @link    http://www.oemr.org
- * @link    http://SuncoastConnection.com
- * @author  Bryan lee <leebc 11 at acm dot org>
- * @author  Art Eaton <art@starfrontiers.org>
+ * Please support this product by sharing your changes with the LibreHealth.io community.
  */
 
 class PQRSReportFactory extends RsReportFactoryAbstract {
