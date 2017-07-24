aae7e752ad56e23ca4f4b923cca42a91b2b80071
Art Eaton <Art@starfrontiers.org>
2017-07-24 12:43:06 -0400

    data import fix

--- modules/MIPS/import_data.php
+++ modules/MIPS/import_data.php
@@ -24,51 +24,51 @@ include_once("$srcdir/api.inc");
 if($_POST['formSubmit'] == "Submit") 
 {
 sqlStatement("TRUNCATE TABLE `addresses`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/addresses.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/addresses.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `billing`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/billing.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/billing.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `facility`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/facilities.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/facilities.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `form_encounter`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/form_encounter.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/form_encounter.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `forms`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/forms.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/forms.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `insurance_companies`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/insurance_companies.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/insurance_companies.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `insurance_data`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/insurance_data.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/insurance_data.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `patient_data`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/patient_data.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/patient_data.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `phone_numbers`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/phone_numbers.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/phone_numbers.sql");
 sqlStatement($query);
 
 sqlStatement("TRUNCATE TABLE `x12_partners`;");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/x12_partners.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/x12_partners.sql");
 sqlStatement($query);
 
 sqlStatement("DELETE FROM `users` WHERE `users`.`id` > '20';");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/users.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/users.sql");
 sqlStatement($query);
 
 sqlStatement("DELETE FROM `groups` WHERE `groups`.`id` > '20';");
-$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/PQRS/dropzone/files/Importer/groups.sql");
+$query = file_get_contents($GLOBALS['OE_SITE_DIR']."/filemanager/files/Importer/groups.sql");
 sqlStatement($query);
 echo "Database updated!";
 }
