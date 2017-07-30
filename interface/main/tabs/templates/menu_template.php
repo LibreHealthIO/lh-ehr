2cbe5e84af6bac9e12ce3d5da6ff6d9043d334ba
Nilesh Prasad <prasadnilesh96@gmail.com>
2017-07-30 10:37:42 +0530

    Navbar Bootstrapped
    dropdown on hover completed
    
    submenu bug resolved
    
    ui changes -- padding decreased

--- interface/main/tabs/templates/menu_template.php
+++ interface/main/tabs/templates/menu_template.php
@@ -1,18 +1,30 @@
+<link rel="stylesheet" type="text/css" href=".../../css/menu.css"/>
 <script type="text/html" id="menu-action">
-    <div class='menuLabel' data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></div>
+    <a data-bind="text:label,click: menuActionClick,css: {menuDisabled: ! enabled()}"></a>
 </script>
+
 <script type="text/html" id="menu-header">
-    
-    <div class="menuSection">
-        <div class='menuLabel' data-bind="text:label"></div>
-        <ul class="menuEntries" data-bind="foreach: children">
-           <li data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>
-        <ul>
-    </div>
+    <a data-bind="text:label" class="dropdown-toggle " data-toggle="dropdown"> <b class="caret"></b></a>
+    <ul class="dropdown-menu" data-bind="foreach: children">        
+        <li class="dropdown dropdown-submenu" data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>
+    </ul>
 </script>
-<script type="text/html" id="menu-template">
-    <div class='appMenu' data-bind="foreach: menu">
-            <span data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></span>
-    </div>
+
+<script type="text/html" id="menu-template">  
+    <!-- Collect the nav links, forms, and other content for toggling -->    
+    <ul class="nav navbar-nav" data-bind="foreach: menu">
+        <li class="dropdown" data-bind="template: {name:header ? 'menu-header' : 'menu-action', data: $data }"></li>            
+    </ul>       
 </script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.nav li.dropdown').hover(function() {
+            $(this).addClass('open');
+        }, function() {
+            $(this).removeClass('open');
+        });
+    });
+</script>
+
 
