317c2c80508cd82d96dba2c5e417503cb7fad23b
Ujjwal Arora <arora.ujjwal@gmail.com>
2017-08-21 02:11:14 +0530

    Insertion and updation

--- modules/calendar/admin.php
+++ modules/calendar/admin.php
@@ -6,19 +6,51 @@ require_once("includes/admin_helper.php");
 // get bootstrap
 call_required_libraries(true, false, false, false);
 
+
+// if update category button is used
+if($_POST['updateCat'] == 1) {
+  // call for creation/updation of category
+  $res = createUpdateCategory($_SESSION['category'], $_POST['catName'], $_POST['catCol'], 
+    $_POST['catDes'], $_POST['catType'], $_POST['catDur'], 
+    $_POST['catAllDay'], $_SESSION['category'] == "__NEW__");
+  $rows = $res->getRows();
+  foreach($rows as $row) {
+    echo $row;
+  }
+}
+
+
+// retrieve available categories from admin_helper
 $categories = getCategories();
+
+// assign POSTed category to session
+if(isset($_POST['category'])) {
+  $_SESSION['category'] = $_POST['category'];
+}
+
+// get category info if we have a category selected
+$selectedCat = array();
+if(isset($_SESSION['category']) && $_SESSION['category']!=NULL) {
+  foreach($categories as $category) {
+    if($category['pc_catid'] == $_POST['category']) {
+      $selectedCat = $category;
+      break;
+    }
+  }
+}
 
 ?>
 
 <html>
 <head>
   <link href="css/admin.css" rel="stylesheet" />
+  <script src="<?php echo $GLOBALS['standard_js_path']; ?>jquery-min-3-1-1/index.js"></script>
 </head>
 <body>
   <div class="container-fluid">
-    
     <div class="row block cat-body">
-      <form class="form-horizontal">
+      
+      <form class="form-horizontal" id="cat-select-form" action="" method="POST">
         <div class="form-group col-xs-12">
           <label for="category">Select Category</label>
           <select class="form-control" id="category" name="category" required>
@@ -27,41 +59,58 @@ $categories = getCategories();
             foreach($categories as $category) {
               $catid = $category['pc_catid'];
               $catname = $category['pc_catname'];
-              echo "<option value='$catid'>$catname</option>";
+              echo "<option value='$catid'"; 
+                if($catid == $_POST['category']) {
+                  echo " selected";
+                }
+              echo ">$catname</option>";
             }
             ?>
           </select>
         </div>
       </form>
+      
     </div>
   
     <div class="row block">
       <div class="cat-head">
-        <h4>New Category</h4>
+        <?php
+          // if our selected category info array isn't empty
+          if(!empty($selectedCat)) {
+            echo "<h4>". $selectedCat['pc_catname'] . "</h4>";
+          } else {
+            echo "<h4>New Category</h4>";
+          }
+        ?>
       </div>
-      <form class="form-horizontal cat-body"  action="" method="POST">
+      <form class="form-horizontal cat-body" action="" method="POST">
         
         <div class="row">
           <div class="form-group col-xs-6">
             <label class="control-label col-md-2" for="catName">Name</label>
             <div class="col-md-10">
-              <input type="text" class="form-control" id="catName" name="catName" placeholder="Category Name" required>
+              <input type="text" class="form-control" id="catName" name="catName" placeholder="Category Name" required 
+              <?php if(!empty($selectedCat)) echo " value=\"" . $selectedCat['pc_catname'] . "\"";  ?> >
             </div>
           </div>
           
           <div class="form-group col-xs-6">
             <label class="control-label col-md-2" for="catCol">Color</label>
             <div class="col-md-10">
-              <input type="color" class="form-control" id="catCol" name="catcol" placeholder="Select a Color" value="#e5e5e5" required>
+              <input type="color" class="form-control" id="catCol" name="catCol" placeholder="Select a Color" required
+              <?php if(!empty($selectedCat)) echo " value=\"" . $selectedCat['pc_catcolor'] . "\"";
+              else echo "value=\"#e5e5e5\"";  ?> >
             </div>
           </div>
         </div>
         
         <div class="row">
           <div class="form-group col-xs-6">
-            <label class="control-label col-md-2" for="catDur">Duration</label>
+            <label class="control-label col-md-2" for="catDur">Duration (Minutes)</label>
             <div class="col-md-10">
-              <input type="text" class="form-control" id="catDur" name="catDur" placeholder="HH:MM" required>
+              <input type="number" class="form-control" id="catDur" name="catDur" placeholder="Minutes" required
+              <?php if(!empty($selectedCat)) echo " value=\"" . $selectedCat['pc_duration']/60 . "\"" ?> 
+              <?php if(!empty($selectedCat) && $selectedCat['pc_end_all_day'] == 1) echo " disabled"; ?> >
             </div>
           </div>
           
@@ -69,10 +118,12 @@ $categories = getCategories();
             <label class="control-label col-md-2" for="catAllDay">All Day</label>
             <div class="col-md-10">
               <div class="radio-inline">
-                <label><input type="radio" name="catAllDay" value="yes" required>Yes</label>
+                <label><input type="radio" id="allDay1" name="catAllDay" value="1" required
+                <?php if(!empty($selectedCat) && $selectedCat['pc_end_all_day'] == 1) echo " checked"; ?>>Yes</label>
               </div>
               <div class="radio-inline">
-                <label><input type="radio" name="catAllDay" value="no" required>No</label>
+                <label><input type="radio" id="allDay0" name="catAllDay" value="0" required
+                <?php if(!empty($selectedCat) && $selectedCat['pc_end_all_day'] == 0) echo " checked"; ?>>No</label>
               </div>
             </div>
           </div>
@@ -82,7 +133,8 @@ $categories = getCategories();
           <div class="form-group col-xs-6">
             <label class="control-label col-md-2" for="catDes">Description</label>
             <div class="col-md-10">
-              <textarea rows="2" class="form-control" id="catDes" name="catDes" placeholder="Category Description" required></textarea>
+              <textarea rows="2" class="form-control" id="catDes" name="catDes" placeholder="Category Description" 
+              required><?php if(!empty($selectedCat)) echo $selectedCat['pc_catdesc']; ?></textarea>
             </div>
           </div>
           
@@ -91,15 +143,23 @@ $categories = getCategories();
             <div class="col-md-10">
               <select class="form-control" id="catType" name="catType" required>
                 <option value="0">Patient</option>
-                <option value="1">Provider</option>
+                <option value="1" <?php if(!empty($selectedCat) && $selectedCat['pc_cattype'] == 1) echo "selected" ?>>Provider</option>
+                <option value="2" <?php if(!empty($selectedCat) && $selectedCat['pc_cattype'] == 2) echo "selected" ?>>Clinic</option>
               </select>
             </div>
           </div>
         </div>
         
-        <div class="form-group"> 
-          <div class="col-sm-12">
-            <button type="submit" class="btn btn-default" name="submit" value="1">Submit</button>
+        <div class="row">
+          <div class="form-group col-xs-9"> 
+            <div class=col-md-12>
+              <button type="submit" class="btn btn-primary" name="updateCat" value="1">Update</button>
+            </div>
+          </div>
+          <div class="form-group col-xs-3">
+            <div class=col-md-12>
+              <button type="submit" class="btn btn-danger" name="deleteCat" value="1">Delete</button>
+            </div>
           </div>
         </div>
         
@@ -107,4 +167,28 @@ $categories = getCategories();
     </div>
   </div>
     
+    <script>
+    // selector for category submits the form
+    $("#category").change(function() { $('#cat-select-form').submit(); });
+    
+    // change card color according to selected color
+    $('.cat-head').css("background", $("#catCol").val());
+    $("#catCol").change(function() { $('.cat-head').css("background", $("#catCol").val());});
+    
+    // disable duration if all day is selected
+    $('#allDay1').click(function () {
+        if ($(this).is(':checked')) {
+          $("#catDur").prop("disabled", true);
+        }
+    });
+    
+    // enale duration if not all day
+    $('#allDay0').click(function () {
+        if ($(this).is(':checked')) {
+          $("#catDur").prop("disabled", false);  
+        }
+    });
+    </script>
+    
 </body>
+</html>
--- modules/calendar/includes/admin_helper.php
+++ modules/calendar/includes/admin_helper.php
@@ -6,5 +6,26 @@ function getCategories() {
   return $res;
 }
 
+function createUpdateCategory($catid, $catname, $catcol, $catdes, $cattype, $durmins, $allday, $new=TRUE) {
+  if($allday == 1) {
+    $duration = 0;
+  } else {
+    $duration = $durmins*60;
+  }
+  
+  if($new) {
+    $query = "INSERT INTO libreehr_postcalendar_categories(pc_catname, 
+      pc_catcolor, pc_catdesc, pc_cattype, pc_duration, pc_end_all_day)
+      VALUES('$catname', '$catcol', '$catdes', $cattype, $duration, $allday);";
+      $res = sqlStatement($query);
+  } else {
+    $query = "UPDATE libreehr_postcalendar_categories SET 
+      pc_catname = '$catname', pc_catcolor = '$catcol', pc_catdesc = '$catdes',
+      pc_cattype = $cattype, pc_duration = $duration, pc_end_all_day = $allday 
+      WHERE pc_catid = $catid;";
+    $res = sqlStatement($query);
+  }
+  return $res;
+}
 
 ?>
