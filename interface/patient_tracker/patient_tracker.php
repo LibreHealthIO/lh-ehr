1872219757b27619932c35734e7b3bc2ce6bb901
Terry Hill <teryhill@yahoo.com>
2017-09-28 11:40:20 -0400

    Cleanup of tracker
    This was moved to the setting section and was not removed.

--- interface/patient_tracker/patient_tracker.php
+++ interface/patient_tracker/patient_tracker.php
@@ -402,11 +402,6 @@ function openNewTopWindow(newpid,newencounterid) {
       <h3><?php echo xlt('Appointment Date'). ' : ' . text($form_from_date) ?></h3>
   <?php } ?>
   <?php } ?>
- <div id= 'inanewwindow'> 
- <input type='hidden' name='setting_new_window' value='1' />
- <input type='checkbox' name='form_new_window' value='1'<?php echo $new_window_checked; ?> /><?php
-  echo xlt('Open Patient in New Window'); ?> 
- </div>
  </div>
 <?php if ($GLOBALS['pat_trkr_timer'] =='0') { ?>
 <table class="table">
