d58c2af563ea8512d1b4cf544643a17e3edfbd01
Art Eaton <Art@starfrontiers.org>
2017-10-25 01:28:43 -0400

    found boo-boo
    I am blaming sublime text editor for all my formatting errors.
    Auto-complete crams brackets right against php tags, and did some other
    evil stuff.  Everything should have worked from the beginning

--- interface/usergroup/usergroup_admin.php
+++ interface/usergroup/usergroup_admin.php
@@ -375,7 +375,7 @@ $form_inactive = empty($_REQUEST['form_inactive']) ? false : true;
 ?>
 <html>
 <head>
-<?php call_required_libraries(array ("jquery-min-1-3-2","bootstrap","fancybox"));
+<?php call_required_libraries(array("jquery-min-1-3-2","bootstrap","fancybox"));
       resolveFancyboxCompatibility();
 ?>
 
