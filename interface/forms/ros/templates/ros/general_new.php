09930069da412828c810ed604b0a1e5ee730d5cb
jdew192837 <jdew192837@gmail.com>
2017-12-28 12:10:13 -0600

    Commented out '<?php html_header_show()?>'

--- interface/forms/ros/templates/ros/general_new.php
+++ interface/forms/ros/templates/ros/general_new.php
@@ -1,5 +1,6 @@
 <html>
   <head>
+  <!-- Function serves no use -->
   <?php html_header_show();?>
   <!-- Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
