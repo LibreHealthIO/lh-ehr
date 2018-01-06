9182d3c59d08c0c878035758ede593e898069485
MigDinny <mikepro2013@gmail.com>
2018-01-06 23:58:01 +0000

    idented and refactored summary_title.php

--- interface/patient_file/summary/summary_title.php
+++ interface/patient_file/summary/summary_title.php
@@ -1,61 +1,54 @@
 <?php
-include_once("../../globals.php");
-require_once($GLOBALS['srcdir'].'/formdata.inc.php');
-include_once("$srcdir/patient.inc");
-include_once("$srcdir/calendar.inc");
-require_once("$srcdir/classes/Pharmacy.class.php");
+  include_once("../../globals.php");
+  require_once($GLOBALS['srcdir'].'/formdata.inc.php');
+  include_once("$srcdir/patient.inc");
+  include_once("$srcdir/calendar.inc");
+  require_once("$srcdir/classes/Pharmacy.class.php");
+  require_once("$srcdir/headers.inc.php");
 ?>
 <html>
-<head>
-<?php html_header_show();?>
-<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
-<script type="text/javascript" src="../../../library/dialog.js"></script>
-
-<script language='JavaScript'>
-
- function newEvt() {
-  dlgopen('<?php echo $GLOBALS["web_root"]; ?>/modules/calendar/add_edit_event.php?patientid=<?php echo $pid ?>',
-   '_blank', 550, 270);
-  return false;
- }
-
-</script>
-
-</head>
-
-<body class="body_title">
-
-<?php
- $result = getPatientData($pid, "fname,lname,pid,phone_home,pharmacy_id,DOB,DATE_FORMAT(DOB,'%Y%m%d') as DOB_YMD");
- $provider_results = sqlQuery("select * from users where username='" . $_SESSION{"authUser"} . "'");
- $age = getPatientAge($result["DOB_YMD"]);
-
- $info = 'ID: ' . $result['pid'];
- if ($result['DOB']) $info .= ', DOB: ' . $result['DOB'] . ', Age: ' . $age;
- if ($result['phone_home']) $info .= ', Home: ' . $result['phone_home'];
-
- if ($result['pharmacy_id']) {
-  $pharmacy = new Pharmacy($result['pharmacy_id']);
-  if ($pharmacy->get_phone()) $info .= ', Pharm: ' . $pharmacy->get_phone();
- }
-?>
-
-<table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
- <tr>
-  <td style="width:45%; vertical-align:middle; white-space: nowrap; text-align:left">
-
-  <span class="title_bar_top"><?php echo $result{"fname"} . " " . $result{"lname"};?></span>
-   
-   <span style="font-size:0.7em;">(<?php echo $info ?>)</span>
-  </td>
-  <td style="width:35%; vertical-align:middle; white-space: nowrap; text-align:center">
-   <span class="title_bar_top"><?php xl('Logged in as','e'); ?>: <?php echo $provider_results{"fname"}.' '.$provider_results{"lname"};?></span>
-  </td>
-  <td style="width:20%; vertical-align:middle; white-space: nowrap; text-align:right">
-   <a href='' class='title_bar_top' onclick='return newEvt()'><?php xl('New Appointment','e'); ?></a>
-  </td>
- </tr>
-</table>
-
-</body>
+  <head>
+    <?php
+      html_header_show();
+    ?>
+    <link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
+    
+    <script language='JavaScript'>
+      function newEvt() {
+       dlgopen('<?php echo $GLOBALS["web_root"]; ?>/modules/calendar/add_edit_event.php?patientid=<?php echo $pid ?>',
+        '_blank', 550, 270);
+       return false;
+      }  
+    </script>
+  </head>
+  <body class="body_title">
+    <?php
+      $result = getPatientData($pid, "fname,lname,pid,phone_home,pharmacy_id,DOB,DATE_FORMAT(DOB,'%Y%m%d') as DOB_YMD");
+      $provider_results = sqlQuery("select * from users where username='" . $_SESSION{"authUser"} . "'");
+      $age = getPatientAge($result["DOB_YMD"]);
+      
+      $info = 'ID: ' . $result['pid'];
+      if ($result['DOB']) $info .= ', DOB: ' . $result['DOB'] . ', Age: ' . $age;
+      if ($result['phone_home']) $info .= ', Home: ' . $result['phone_home'];
+      
+      if ($result['pharmacy_id']) {
+       $pharmacy = new Pharmacy($result['pharmacy_id']);
+       if ($pharmacy->get_phone()) $info .= ', Pharm: ' . $pharmacy->get_phone();
+      }
+      ?>
+    <table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
+      <tr>
+        <td style="width:45%; vertical-align:middle; white-space: nowrap; text-align:left">
+          <span class="title_bar_top"><?php echo $result{"fname"} . " " . $result{"lname"};?></span>
+          <span style="font-size:0.7em;">(<?php echo $info ?>)</span>
+        </td>
+        <td style="width:35%; vertical-align:middle; white-space: nowrap; text-align:center">
+          <span class="title_bar_top"><?php xl('Logged in as','e'); ?>: <?php echo $provider_results{"fname"}.' '.$provider_results{"lname"};?></span>
+        </td>
+        <td style="width:20%; vertical-align:middle; white-space: nowrap; text-align:right">
+          <a href='' class='title_bar_top' onclick='return newEvt()'><?php xl('New Appointment','e'); ?></a>
+        </td>
+      </tr>
+    </table>
+  </body>
 </html>
