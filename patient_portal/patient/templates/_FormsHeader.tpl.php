cfe2b840a6fe073b8b30e1b3a781ebfd89e1ebb9
Terry Hill <teryhill@yahoo.com>
2018-01-26 01:57:22 -0500

    Newest version of portal. (#957)
    This version allows for the patient to self register.

--- patient_portal/patient/templates/_FormsHeader.tpl.php
+++ patient_portal/patient/templates/_FormsHeader.tpl.php
@@ -22,32 +22,31 @@
 <meta content="width=device-width, initial-scale=1, user-scalable=no"
     name="viewport">
 
-<base href="<?php $this->eprint($this->ROOT_URL); ?>" />
 <meta name="description" content="LibreEHR Portal" />
 <!-- <meta name="author" content="Form | sjpadgett@gmail.com" /> -->
 
 <!-- Styles -->
-<link href="<?php echo $GLOBALS['standard_js_path']; ?>/bootstrap-3-3-4/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="<?php echo $GLOBALS['standard_js_path']; ?>bootstrap-3-3-4/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
 <?php if ($_SESSION['language_direction'] == 'rtl') { ?>
-    <link href="<?php echo $GLOBALS['standard_js_path']; ?>/bootstrap-rtl-3-3-4/dist/css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css" />
+    <link href="<?php echo $GLOBALS['standard_js_path']; ?>bootstrap-rtl-3-3-4/dist/css/bootstrap-rtl.min.css" rel="stylesheet" type="text/css" />
 <?php } ?>
 
-<link href="<?php echo $GLOBALS['fonts_path']; ?>/font-awesome-4-6-3/css/font-awesome.min.css" rel="stylesheet" />
-<link href="<?php echo $GLOBALS['standard_js_path']; ?>/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.min.css"  rel="stylesheet" />
-<link href="styles/style.css" rel="stylesheet" />
+<link href="<?php echo $GLOBALS['fonts_path']; ?>font-awesome-4-6-3/css/font-awesome.min.css" rel="stylesheet" />
+<link href="<?php echo $GLOBALS['standard_js_path']; ?>jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.min.css"  rel="stylesheet" />
+<link href="<?php echo $GLOBALS['web_root']; ?>/patient_portal/patient/styles/style.css" rel="stylesheet" />
 
-<script type="text/javascript" src="scripts/libs/LAB.min.js"></script>
+<script type="text/javascript" src="<?php echo $GLOBALS['web_root']; ?>/patient_portal/patient/scripts/libs/LAB.min.js"></script>
 <script type="text/javascript">
-            $LAB.script("<?php echo $GLOBALS['standard_js_path']; ?>/jquery-min-1-11-3/index.js").wait()
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/bootstrap-3-3-4/dist/js/bootstrap.min.js")
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/emodal-1-2-65/dist/eModal.js")
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/moment-2-13-0/moment.js")
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.full.min.js")
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/underscore-1-8-3/underscore-min.js").wait()
-                .script("<?php echo $GLOBALS['standard_js_path']; ?>/backbone-1-3-3/backbone-min.js")
-                .script("scripts/app.js")
-                .script("scripts/model.js").wait()
-                .script("scripts/view.js").wait()
+            $LAB.script("<?php echo $GLOBALS['standard_js_path']; ?>jquery-min-1-11-3/index.js").wait()
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>bootstrap-3-3-4/dist/js/bootstrap.min.js")
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>emodal-1-2-65/dist/eModal.js")
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>moment-2-13-0/moment.js")
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>jquery-datetimepicker-2-5-4/build/jquery.datetimepicker.full.min.js")
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>underscore-1-8-3/underscore-min.js").wait()
+                .script("<?php echo $GLOBALS['standard_js_path']; ?>backbone-1-3-3/backbone-min.js")
+                .script("<?php echo $GLOBALS['web_root']; ?>/patient_portal/patient/scripts/app.js")
+                .script("<?php echo $GLOBALS['web_root']; ?>/patient_portal/patient/scripts/model.js").wait()
+                .script("<?php echo $GLOBALS['web_root']; ?>/patient_portal/patient/scripts/view.js").wait()
         </script>
 
 </head>
