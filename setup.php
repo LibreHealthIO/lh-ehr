67efa67269c4a67e42bfe1facf82048e697315b8
naveen <kmnaveen101@gmail.com>
2018-03-01 22:20:27 +0530

    favicon added to setup (#1034)

--- setup.php
+++ setup.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 <?php
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
