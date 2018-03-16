0704930b78576cd5567c0b6fadc75ac2d3e2dd4d
Apoorv Choubey <theapoorvs1@gmail.com>
2018-03-17 00:48:25 +0530

    Fix for #1061: User edit UI inaccessible (#1062)
    * Fix for #1061
    
    * Second fix for # 1061
    
    * Third fix for #1061

--- library/headers.inc.php
+++ library/headers.inc.php
@@ -1,21 +1,21 @@
 <?php
 
-/** 
+/**
  *  Common include pathing and related native LHEHR functions
- * 
+ *
  * Copyright (C) 2017 Art Eaton
  * SOURCE:  Ken Chapple at mi-squared.com
- * 
+ *
  * LICENSE: This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0
- * See the Mozilla Public License for more details. 
+ * See the Mozilla Public License for more details.
  * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
- * 
- * @package Librehealth EHR 
+ *
+ * @package Librehealth EHR
  * @author Art Eaton <art@suncoastconnection.com>
  * @link http://librehealth.io
- *  
+ *
  * Please help the overall project by sending changes you make to the author and to the LibreEHR community.
- * 
+ *
  */
 
 ?>
@@ -41,24 +41,24 @@
     }
 ?>
 
-<?php 
-/*  
+<?php
+/*
     This function can be used to call various frequently used libraries.
     Parameters for this function are passed in an unkeyed array of strings.
-    Strings equate to the directory name in the /assets/ directory.   
+    Strings equate to the directory name in the /assets/ directory.
 */
 function call_required_libraries($library_array){
     /* First checking for any library in a directory matching the string "/jquery-min-/".
      * When one is found, use that value in the URL string, then add "index.js"
      */
 
-    foreach ($library_array as $v){ 
+    foreach ($library_array as $v){
         if (preg_match("/jquery-min-/", $v)) {?>
-            <script type="text/javascript" src="<?php echo $GLOBALS['standard_js_path'].$v ; ?>/index.js"></script>    
-        <?php 
+            <script type="text/javascript" src="<?php echo $GLOBALS['standard_js_path'].$v ; ?>/index.js"></script>
+        <?php
         }
-    }   
-        
+    }
+
     if (in_array("bootstrap", $library_array)) {   ?>
         <link rel="stylesheet" href="<?php echo $GLOBALS['standard_js_path']; ?>bootstrap-3-3-4/dist/css/bootstrap.min.css" type="text/css">
         <script type="text/javascript" src="<?php echo $GLOBALS['standard_js_path']; ?>bootstrap-3-3-4/dist/js/bootstrap.min.js"></script>
@@ -73,6 +73,11 @@ function call_required_libraries($library_array){
     if (in_array("fancybox-addpatient",$library_array)){   ?>
         <link rel="stylesheet" href="<?php echo $GLOBALS['css_path']; ?>fancybox-addpatient/jquery.fancybox-1.2.6.css" media="screen" />
         <script type="text/javascript" src="<?php echo $GLOBALS['standard_js_path']; ?>fancybox-addpatient/jquery.fancybox-1.2.6.js"></script>
+    <?php
+    }
+    if (in_array("fancybox-custom",$library_array)){   ?>
+        <link rel="stylesheet" href="<?php echo $GLOBALS['css_path']; ?>fancybox-custom/jquery.fancybox-1.2.6.css" media="screen" />
+        <script type="text/javascript" src="<?php echo $GLOBALS['standard_js_path']; ?>fancybox-custom/jquery.fancybox-1.2.6.js"></script>
     <?php
     }
     if (in_array("knockout",$library_array)){   ?>
@@ -113,13 +118,13 @@ function call_required_libraries($library_array){
         <link rel="stylesheet" type="text/css" href="<?php echo $GLOBALS['css_path']; ?>select2/select2.min.css">
     <?php
     }
-    
+
 }
 ?>
 
 <?php
 /*
-    This function resolves the Console-error "Uncaught TypeError: Cannot read property 'msie' of undefined" . This error comes up when using 
+    This function resolves the Console-error "Uncaught TypeError: Cannot read property 'msie' of undefined" . This error comes up when using
     fancybox-1.2.6 and fancybox-1.3.4 versions with jQuery version 3.1.1. It is because the $.browser method was removed in jQuery 1.9.
 */
 function resolveFancyboxCompatibility() { ?>
@@ -133,8 +138,8 @@ function resolveFancyboxCompatibility() { ?>
                 jQuery.browser.version = RegExp.$1;
             }
         })();
-    </script>    
-<?php 
+    </script>
+<?php
 }
 // always include this when headers is included
 // (don't know if it's a good practice because this is included even before <html>)
