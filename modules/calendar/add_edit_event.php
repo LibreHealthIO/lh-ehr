7d9c48a8f20e311bf8cffa7fcb13c2499b9f4db3
Prabhakar Prasad <prabhakarpd7284@gmail.com>
2018-03-21 04:15:26 +0530

    Check for clinic hours in creating calender events

--- modules/calendar/add_edit_event.php
+++ modules/calendar/add_edit_event.php
@@ -230,6 +230,15 @@ if ($_POST['form_action'] == "duplicate" || $_POST['form_action'] == "save")
         if ($_POST['form_ampm'] == '2' && $tmph < 12) $tmph += 12;
         $duration = abs($_POST['form_duration']); // fixes #395
     }
+       
+     if($tmph*60+$tmpm > $GLOBALS['schedule_end']*60 or $tmph < $GLOBALS['schedule_start']){
+        echo "<html>\n<body>\n<script language='JavaScript'>\n";
+        echo 'alert("Please select time between clinic hours.");';
+        echo " window.close();\n";
+        echo "</script>\n</body>\n</html>\n";
+        exit();    
+     }
+     
     $tmph = sprintf( "%02d", $tmph );
     $tmpm = sprintf( "%02d", $tmpm );
     $starttime = "$tmph:$tmpm:00";
