80c445a78e38dc54dc1967cada3d98214d00254a
rachmann <muarachmann@gmail.com>
2018-04-27 08:25:22 +0000

    iziModal to fancy

--- interface/main/dated_reminders/dated_reminders_add.php
+++ interface/main/dated_reminders/dated_reminders_add.php
@@ -178,8 +178,7 @@ if(isset($_GET['mID']) and is_numeric($_GET['mID'])){
     <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/dialog.js"></script>  
     <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/common.js"></script>    
     <script type="text/javascript" src="<?php echo $GLOBALS['webroot'] ?>/library/js/jquery-calendar.js"></script>
-   <?php call_required_libraries(array("jquery-min-3-3-1","font-awesome", "iziModalToast"));
-   ?>
+   <?php call_required_libraries(array("jquery-min-3-3-1","font-awesome", "iziModalToast")); ?>
     <script language="JavaScript">
       $(document).ready(function (){   
         
