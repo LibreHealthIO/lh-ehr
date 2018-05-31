992dd16b6a0b3977f074fdb1bcabea7e8018abf9
Apoorv Choubey <theapoorvs1@gmail.com>
2018-06-01 01:03:16 +0530

    refactor refresh code

--- modules/calendar/add_edit_event.php
+++ modules/calendar/add_edit_event.php
@@ -197,16 +197,37 @@ function DOBandEncounter()
  function refreshCalendar() {
     echo "<script type='text/javascript'>";
       echo "var docRoot = top.$('#mainBox');
-            //Calendar frame opened during initial loading of EHR
-            var calFrame1 = docRoot.find('iframe[name=" . "lst" . "]');
-            var calSrc1 = calFrame1.attr('src');
-            if (calSrc1 !== undefined) {
-                calFrame1.attr('src', calSrc1);
+            //get references to all possible frames for calendar
+            var leftFrame = docRoot.find('iframe[name=" . "lst" . "]'); //initial loading of EHR
+            var rightFrame = docRoot.find('iframe[name=" . "pat" . "]'); //initial loading of EHR
+            var calFrame = docRoot.find('iframe[name=" . "cal" . "]'); //via main menu
+
+            var calFrameStr = 'interface/main/main_info.php'; //url of Calendar Screen used for identification
+
+            //get source string of frames
+            var leftFrameSrc = leftFrame.attr('src');
+            var rightFrameSrc = rightFrame.attr('src');
+            var calFrameSrc = calFrame.attr('src');
+
+            //when Calendar is opened via main menu
+            if (calFrameSrc !== undefined) {
+                calFrame.attr('src', calFrameSrc); //refresh frame
             } else {
-                //Calendar frame opened via main menu
-                var calFrame2 = docRoot.find('iframe[name=" . "cal" . "]');
-                var calSrc2 = calFrame2.attr('src');
-                calFrame2.attr('src', calSrc2);
+                //when Calendar is opened in one of two initially loaded frames
+
+                //if opened in left frame
+                if (leftFrameSrc !== undefined) {
+                    if (leftFrameSrc.indexOf(calFrameStr) !== -1) {
+                        leftFrame.attr('src', leftFrameSrc); //refresh frame
+                    }
+                }
+
+                //if opened in right frame
+                if (rightFrameSrc !== undefined) {
+                    if (rightFrameSrc.indexOf(calFrameStr) !== -1) {
+                        rightFrame.attr('src', rightFrameSrc); //refresh frame
+                    }
+                }
             }";
     echo "</script>";
  }
