9d699aaae64005ac805be6588a99af6caf2d34e5
Art Eaton <Art@starfrontiers.org>
2018-06-13 14:03:28 -0400

    Favicon.ico 404 errors (#1151)
    no "public" directory exists.  Probably from some composer implementation.  dunno.  Fixed.

--- sql_patch.php
+++ sql_patch.php
@@ -33,7 +33,7 @@ $EMRversion = trim(preg_replace('/\s*\([^)]*\)/', '', $GLOBALS['libreehr_version
 <head>
 <title>LibreHealth EHR <?php echo attr($EMRversion) ?> <?php echo xlt('Database Patch'); ?></title>
 <link rel='STYLESHEET' href='interface/themes/style_setup.css'>
-<link rel="shortcut icon" href="public/images/favicon.ico" />
+<link rel="shortcut icon" href="favicon.ico" />
 </head>
 <body style="color:green;">
 
