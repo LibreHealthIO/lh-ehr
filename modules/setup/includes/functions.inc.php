6983d6342bc4668d8cb391f65a9d225832e9779d
muarachmann <muarachmann@gmail.com>
2018-06-20 06:07:49 +0000

    Update functions.inc.php

--- modules/setup/includes/functions.inc.php
+++ modules/setup/includes/functions.inc.php
@@ -135,7 +135,7 @@
 
               if($fail) {
                   echo '<p><strong>Your server does not meet the following requirements in order to install LibreEHR.</strong>';
-                  echo '<br>The following requirements failed, please contact your hosting provider in order to receive assistance with meeting the system requirements for Magento:';
+                  echo '<br>The following requirements failed, please contact your hosting provider in order to receive assistance with meeting the system requirements for LibreHealth:';
                   echo '<ul>'.$fail.'</ul></p>';
                   echo 'The following requirements were successfully met:';
                   echo '<ul>'.$pass.'</ul>';
