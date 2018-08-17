ffb200d031f3e872e5754f38c2afad31f726f717
Trodrige <tigrodrige@gmail.com>
2018-08-17 15:58:17 +0100

    seed the database with default report formats and system features

--- modules/report_generator/Modules/ReportGenerator/Database/Seeders/ReportGeneratorDatabaseSeeder.php
+++ modules/report_generator/Modules/ReportGenerator/Database/Seeders/ReportGeneratorDatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Modules\ReportGenerator\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 
 class ReportGeneratorDatabaseSeeder extends Seeder
@@ -15,8 +16,12 @@ class ReportGeneratorDatabaseSeeder extends Seeder
     public function run()
     {
         Model::unguard();
-        $this->call(DraggableComponentTableSeeder::class);
-        $this->call(ReportFormatTableSeeder::class);
-        // $this->call("OthersTableSeeder");
+        //$this->call(DraggableComponentTableSeeder::class);
+        //$this->call(ReportFormatTableSeeder::class);
+
+        // Seed the database with default system features and report formats.
+        $path = './../../../../Documentation/librereportgenerator.sql';
+        DB::unprepared(file_get_contents($path));
+        $this->command->info('Report generator tables seeded!')
     }
 }
