18de656b827fd9606f46197f9c2e73faed251ca4
Stevan Marjanovic <marjanovicstevan@outlook.com>
2018-10-31 01:08:33 +0100

    Reset $config in sites/default/sqlconf.php
    I think this solves the problem referred by @muarachmann in #1270

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -18,12 +18,6 @@ $sqlconf["port"] = $port;
 $sqlconf["login"] = $login;
 $sqlconf["pass"] = $pass;
 $sqlconf["dbase"] = $dbase;
-/////////WARNING!/////////
-//Setting $config to = 0//
-// will break this site //
-//and cause SETUP to run//
-$config = 1; /////////////
-//////////////////////////
-//////////////////////////
-//////////////////////////
-?>
+
+//$config for default site files should never be changed.
+$config = 0; 
