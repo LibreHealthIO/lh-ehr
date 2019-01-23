afbd85c07e1709ea441d6d8404f58f52ea75ec1e
Stevan Marjanovic <marjanovicstevan@outlook.com>
2019-01-23 19:50:33 +0100

    Fixed the sqlconf.php issue (#1273)

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 // Set the site ID if required.  This must be done before any database
 // access is attempted.
 
-if (!empty($_GET['site']))
+if (!empty($_GET['site'])) {
     $site_id = $_GET['site'];
-else if (is_dir("sites/" . $_SERVER['HTTP_HOST']))
+} elseif (is_dir("sites/" . $_SERVER['HTTP_HOST'])) {
     $site_id = $_SERVER['HTTP_HOST'];
-else
+} else {
     $site_id = 'default';
+}
 
 if (empty($site_id) || preg_match('/[^A-Za-z0-9\\-.]/', $site_id))
     die("Site ID '".htmlspecialchars($site_id,ENT_NOQUOTES)."' contains invalid characters.");
@@ -22,14 +23,12 @@ require_once "sites/$site_id/sqlconf.php";}
 
 if ($config == 1) {
     header("Location: interface/login/login.php?site=$site_id");
-} else {
-    if ($site_id != 'default'){
+} elseif ($site_id != 'default') {
     header("Location: setup.php");
-    }else{
-        echo "<p>Direct configuration of Site ID \" ".$site_id." \" is not permitted.</p>".
-        "<p>If you know the name of the EHR site you want to access<br>".
-        "Try adding \"?site=<i>mysitename</i>\" to the URL address.</p>";
-        echo "<p>If you have not yet installed LibreEHR, or wish to install a new site, you may <br>".
-        " <a href='setup.php'>CLICK HERE</a> to do so.</p>";
-    }
+} else {
+    echo "<p>Direct configuration of Site ID \" ".$site_id." \" is not permitted.</p>".
+    "<p>If you know the name of the EHR site you want to access<br>".
+    "Try adding \"?site=<i>mysitename</i>\" to the URL address.</p>";
+    echo "<p>If you have not yet installed LibreEHR, or wish to install a new site, you may <br>".
+    " <a href='setup.php'>CLICK HERE</a> to do so.</p>";
 }
