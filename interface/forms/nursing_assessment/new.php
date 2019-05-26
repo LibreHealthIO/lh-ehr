cfc06f72bdb40c49de4b4a56e1c02b733b021680
elizabeth <asobingai@gmail.com>
2019-05-27 00:40:37 +0100

    add form fields

--- interface/forms/nursing_assessment/new.php
+++ interface/forms/nursing_assessment/new.php
@@ -16,16 +16,91 @@ $returnurl = 'encounter_top.php';
 		<link rel="stylesheet" href="<?php echo $css_header;?>" type="text/css">
 	</head>
 <body>
-	<form method=post action="<?php echo $rootdir;?>/forms/nursing_assessment/save.php?mode=new" name="my_form">
-		<fieldset>
-			<input type="text" name="">
-			<input type="checkbox" name="">
-		</fieldset>
+	<form class="form-horizontal" method=post action="<?php echo $rootdir;?>/forms/nursing_assessment/save.php?mode=new" name="my_form">
+		<div class="row">
+			<!-- assessment section -->
+			<div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title"><?php xl('Assessment','e'); ?></h3>
+					</div>
+					<div class="panel-body">
+						<div class="form-group">
+					    	<label class="control-label col-sm-5" for="ass_comments">Comments/Remarks</label>
+					    	<div class="col-sm-5">
+					    		<textarea class="form-control" id="ass_comments" name="ass_comments"></textarea>
+					    	</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			<!-- Provider notification section -->
+			<div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title"><?php xl('Provider Notification','e'); ?></h3>
+					</div>
+					<div class="panel-body">
+						<div class="form-group">
+					    	<label class="control-label col-sm-5" for="PN_reason">Reason for  Communication</label>
+					    	<div class="col-sm-5">
+					    		<textarea name="PN_reason" class="form-control" id="PN_reason"></textarea>
+					    	</div>
+						</div>
+						<div class="form-group">
+					    	<label  class="control-label col-sm-4" for="PN_role">Provider Name/role</label>
+					    	<div class="col-sm-6">
+					    		<textarea name="PN_role" class="form-control" id="PN_role"></textarea>
+					    	</div>
+						</div>
+						<div class="form-group">
+					    	<label class="control-label col-sm-5" for="PN_method">Method of Communication</label>
+					    	<div class="col-sm-5">
+					    		<select name="PN_method" class="form-control" id="PN_method">
+					    		<option>[select]</option>
+							    <option>Call</option>
+							    <option>Page</option>
+							    <option>Face to Face</option>
+							    <option>Other</option>
+							</select>
+					    	</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
 
-		<fieldset>
-			<input type="radio" name="">
-			<input type="select" name="">
-		</fieldset>
+			<!-- section for vitals -->
+			<div class="col-md-3">
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h3 class="panel-title"><?php xl('Vitals','e'); ?></h3>
+					</div>
+					<div class="panel-body">
+						<div class="form-group">
+					    	<label class="control-label col-sm-4" for="V_temperature">Temperature (&#8451;)</label>
+					    	<div class="col-sm-6">
+					    		<input type="number" name="V_temperature" class="form-control" id="V_temperature">
+					    	</div>
+						</div>
+						<div class="form-group">
+							<label class="control-label col-sm-4" for="">
+								
+							</label>
+
+							<div class="col-sm-6">
+								
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+			<div class="col-md-3">
+				
+			</div>
+		</div>
 
 		<div>
 			<!-- Save/Cancel buttons -->
