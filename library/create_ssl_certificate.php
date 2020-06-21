ce433ed5d29d39a2e4f40319ba50228fba078964
Elizabeth <asobingai@gmail.com>
2020-06-21 22:47:02 +0100

    more fixes for sql injection

--- library/create_ssl_certificate.php
+++ library/create_ssl_certificate.php
@@ -119,7 +119,7 @@ function create_user_certificate($commonName, $emailAddress, $serial, $cacert, $
 
     /* user id is used as serial number to sign a certificate */
     $serial = 0;
-    $res = sqlStatement("select id from users where username='".$commonName."'");
+    $res = sqlStatement("select id from users where username= ? ", array($commonName));
     if ($row = sqlFetchArray($res)) {
         $serial = $row['id'];
     }
