144776eb0550178c57716efbf733d00aa6b45c89
SharonA10 <114970450+SharonA10@users.noreply.github.com>
2022-11-15 14:13:22 -0500

    Update rx_left.php (#1680)
    Added Back to Patient button on the Prescriptions tab.

--- interface/patient_file/summary/rx_left.php
+++ interface/patient_file/summary/rx_left.php
@@ -16,6 +16,9 @@ include_once("../../globals.php");
 <span><?php xl('List', 'e');?></span></a>
 <a href="<?php echo $GLOBALS['webroot']?>/controller.php?prescription&edit&id=&pid=<?php echo $pid?>"  target='RxRight' class="css_button" onclick="top.restoreSession()">
 <span><?php xl('Add','e');?></span></a>
+<a href="../summary/demographics.php" class="css_button" onclick="top.restoreSession()">
+<span>Back To Patient</span>
+</a>
 </td>
 </tr>
 </table>
